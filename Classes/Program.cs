e28466b031b8c447d31e95cf3d72a74b5d357b31
Peter Kalinowski <petekal1323@gmail.com>
2025-04-07 17:49:34 -0400

    exercise 1 complete

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var carOne = new Car(); 
+            carOne.Make = "Toyota";
+            carOne.Model = "Corolla";
+            carOne.Year = 2020;
+            
+            Console.WriteLine($"This is a {carOne.Year} {carOne.Make} {carOne.Model}");
         }
     }
 }
